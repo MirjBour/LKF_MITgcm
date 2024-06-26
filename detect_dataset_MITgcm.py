8918ad8649510598fa268e50dacaa6e1d368a064
Eva Lemaire <160484337+evalmr@users.noreply.github.com>
2024-06-26 18:30:52 +0200

    Add files via upload

--- detect_dataset_MITgcm.py
+++ detect_dataset_MITgcm.py
@@ -9,43 +9,54 @@ from dataset import *
 from lkf_stats_tools import *
 import pickle
 #%% Logical Functions
-def create_nc_file(a_ice_int, h_ice_int, u_ice_int, v_ice_int, shr_ice_int, div_ice_int, vor_ice_int, int_lons, int_lats, #int_dxu, int_dyv, 
-                    ntimesteps, name):
-    ds = nc.Dataset(name, 'w', format='NETCDF4')
-    x = ds.createDimension('x', np.shape(a_ice_int)[2])
-    y = ds.createDimension('y', np.shape(a_ice_int)[1])
-    time = ds.createDimension('time', ntimesteps)
-    x = ds.createVariable('x', 'f4', ('x',))
-    y = ds.createVariable('y', 'f4', ('y',))
-    time = ds.createVariable('time', 'f4', ('time'))
-    a = ds.createVariable('A', 'f4', ('time','y','x'))
-    h = ds.createVariable('H', 'f4', ('time','y','x'))
-    u = ds.createVariable('U', 'f4', ('time','y','x'))
-    v = ds.createVariable('V', 'f4', ('time','y','x'))
-    div = ds.createVariable('div', 'f4', ('time','y','x'))
-    shr = ds.createVariable('shr', 'f4', ('time','y','x'))
-    vor = ds.createVariable('vor', 'f4', ('time','y','x'))
-    lon = ds.createVariable('ULON', 'f4', ('y','x'))
-    lat = ds.createVariable('ULAT', 'f4', ('y','x'))
-    #dxu = ds.createVariable('DXU', 'f4', ('y','x'))
-    #dyv = ds.createVariable('DYV', 'f4', ('y','x'))
-    x[:] = np.arange(np.shape(a_ice_int)[2],dtype='int')
-    y[:] = np.arange(np.shape(a_ice_int)[1],dtype='int')
-    time[:] = np.arange(ntimesteps)
-    a[:,:,:] = a_ice_int
-    h[:,:,:] = h_ice_int
-    u[:,:,:] = u_ice_int
-    v[:,:,:] = v_ice_int
-    shr[:,1:-1,1:-1] = shr_ice_int
-    div[:,1:-1,1:-1] = div_ice_int
-    vor[:,:-1,:-1] = vor_ice_int
-    lon[:,:] = int_lons
-    lat[:,:] = int_lats
-    #dxu[:,:-1] = int_dxu
-    #dyv[1:,:] = int_dyv
-    ds.close()
+def create_var_files(IceDiags_path):
+    """
+    Reads the IceDiags binary files and returns SIuice, SIvice and SIarea binary files.
+    IceDiags_path: path to the binary IceDiags files
+    """
+    output_files = [ifile for ifile in os.listdir(IceDiags_path) if ifile.startswith('IceDiags') and ifile.endswith('.data')]
+    output_files.sort()
+    for i in range (len(output_files)) :
+        ice_diags = xmitgcm.open_mdsdataset(IceDiags_path, prefix="IceDiags", iters=output_files[i].split('.')[1])
+        
+        SIarea = ice_diags["SIarea"]
+        SIuice = ice_diags["SIuice"]
+        SIvice = ice_diags["SIvice"]
+        
+        # Convertir les DataArrays en tableaux NumPy
+        SIarea_np = SIarea.values
+        SIuice_np = SIuice.values
+        SIvice_np = SIvice.values
+        
+        # Vérifier l'ordre des octets et le modifier si nécessaire
+        if SIarea_np.dtype.byteorder == '>':
+            SIarea_np = SIarea_np.byteswap().newbyteorder()
+        if SIuice_np.dtype.byteorder == '>':
+            SIuice_np = SIuice_np.byteswap().newbyteorder()
+        if SIvice_np.dtype.byteorder == '>':
+            SIvice_np = SIvice_np.byteswap().newbyteorder()
+    
+        # Spécifiez le chemin et le nom des fichiers de sortie pour SIarea, SIuice et SIvice
+        SIarea_output_file = IceDiags_path + "SIarea."+ output_files[i].split('.')[1] +".data"
+        SIuice_output_file = IceDiags_path + "SIuice."+ output_files[i].split('.')[1] +".data"
+        SIvice_output_file = IceDiags_path + "SIvice."+ output_files[i].split('.')[1] +".data"
+
+        # Maintenant, vous pouvez les écrire dans les fichiers de sortie respectifs
+        rw.writefield(SIarea_output_file, SIarea_np)
+        rw.writefield(SIuice_output_file, SIuice_np)
+        rw.writefield(SIvice_output_file, SIvice_np)
 
 def compute_strains(dxC,dyC,dyG,dxG, U, V, A):
+    """
+    Computes divergence, vorticity and shear strains from sea ice velocities, concentration and grid parameters.
+    dxC : cell x size at u location
+    dyC : cell y size at v location
+    dyG : cell y size at u location
+    dxG : cell x size at v location
+    U   : zonal ice velocity, >0 from West to East
+    V   : meridional ice velocity, >0 from South to North
+    A   : Fractionnal ice-covered area [0 to 1]
+    """
     dxF = 0.5*(dxG[1:-1,1:-1]+dxG[1:-1,2:])
     dyF = 0.5*(dyG[1:-1,1:-1]+dyG[2:,1:-1])
     dyU = 0.5*(dyG[1:,:-1]+dyG[1:,1:])
@@ -128,7 +139,66 @@ def get_lonlat_MITgcm(output_dir):
     lat = ds.LATC.values
     return(lon,lat)
 
+def create_nc_file(a_ice_int, h_ice_int, u_ice_int, v_ice_int, shr_ice_int, div_ice_int, vor_ice_int, int_lons, int_lats, #int_dxu, int_dyv, 
+                    ntimesteps, name):
+    """
+    Create a netCDF file from :
+    a_ice_int   : Sea ice area
+    h_ice_int   : Sea ice thickness
+    u_ice_int   : Sea ice zonal velocity
+    v_ice_int   : Sea ice meridional velocity
+    shr_ice_int : Sea ice shear strain
+    div_ice_int : Sea ice divergent strain
+    vor_ice_int : Sea ice vorticity
+    int_lons    : Longitude
+    int_lats    : Latitude
+    ntimesteps  : Timestep number
+    name        : name of the netCDF output file
+    return : netCDF file named 'name'
+    """
+    ds = nc.Dataset(name, 'w', format='NETCDF4')
+    x = ds.createDimension('x', np.shape(a_ice_int)[2])
+    y = ds.createDimension('y', np.shape(a_ice_int)[1])
+    time = ds.createDimension('time', ntimesteps)
+    x = ds.createVariable('x', 'f4', ('x',))
+    y = ds.createVariable('y', 'f4', ('y',))
+    time = ds.createVariable('time', 'f4', ('time'))
+    a = ds.createVariable('A', 'f4', ('time','y','x'))
+    h = ds.createVariable('H', 'f4', ('time','y','x'))
+    u = ds.createVariable('U', 'f4', ('time','y','x'))
+    v = ds.createVariable('V', 'f4', ('time','y','x'))
+    div = ds.createVariable('div', 'f4', ('time','y','x'))
+    shr = ds.createVariable('shr', 'f4', ('time','y','x'))
+    vor = ds.createVariable('vor', 'f4', ('time','y','x'))
+    lon = ds.createVariable('ULON', 'f4', ('y','x'))
+    lat = ds.createVariable('ULAT', 'f4', ('y','x'))
+    #dxu = ds.createVariable('DXU', 'f4', ('y','x'))
+    #dyv = ds.createVariable('DYV', 'f4', ('y','x'))
+    x[:] = np.arange(np.shape(a_ice_int)[2],dtype='int')
+    y[:] = np.arange(np.shape(a_ice_int)[1],dtype='int')
+    time[:] = np.arange(ntimesteps)
+    a[:,:,:] = a_ice_int
+    h[:,:,:] = h_ice_int
+    u[:,:,:] = u_ice_int
+    v[:,:,:] = v_ice_int
+    shr[:,1:-1,1:-1] = shr_ice_int
+    div[:,1:-1,1:-1] = div_ice_int
+    vor[:,:-1,:-1] = vor_ice_int
+    lon[:,:] = int_lons
+    lat[:,:] = int_lats
+    #dxu[:,:-1] = int_dxu
+    #dyv[1:,:] = int_dyv
+    ds.close()
+
 def gen_lkf_dataset(root,simu,datatype,mask_rgps=False):
+    """
+    Compute statistics over a LKF dataset generated from a netCDF file
+    root : path to your root dir
+    simu : directory in which the netCDF file is
+    datatype : type of data (can be 'sirex','rgps','mitgcm_2km','mosaic' or 'mitgcm_4km)
+    mask_rgps : True if you want the simulation data to be selected only over the area covered by RGPS observations on the same day 
+    return : lkf dataset object
+    """
     if mask_rgps == True :
         nc_path = root + simu + '/MITgcm_4km_'+ simu[4:] + '.nc'
         output_path = root + simu +'/Dataset_Detected_LKFs'
@@ -166,6 +236,7 @@ use_SIshear = False # True if you want to use the SIshear output files (recommen
 
 for simu in simus :
     output_dir = root + simu + '/' # path to the MITgcm output files
+    create_var_files(output_dir) # Createds SIuice, SIvice, SIarea files from IceDiags files
     ds = open_mdsdataset(output_dir)
 
     # Compute div, shr and vor
